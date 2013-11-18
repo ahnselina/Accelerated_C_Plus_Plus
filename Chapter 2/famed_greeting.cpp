6ddac031ac8b19aa89bef8f94fbaa0914c2d6496
ahnselina <ahnselina@qq.com>
2013-11-18 14:47:40 +0800

    Chapter 2
    the programs of Chapter 2

---
+++ Chapter 2/famed_greeting.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using std::cout;	using std::cin;
+using std::endl;	using std::string;
+
+int main(void)
+{
+    //prompt
+	cout << "please enter your first name: ";
+	//read the name
+	string name;
+	cin >> name;
+	
+	//produce the greeting
+	const string greeting = "Hello, " + name +"!";
+	
+	const int pad = 1;
+	const int rows = pad*2 + 3;
+	const int cols = greeting.size() + pad*2 + 2;
+	
+	cout << endl;
+	
+	//output the framed greeting
+	for(int r=0; r!=rows; ++r)
+	{
+	    string::size_type c = 0;
+	    while(c != cols)
+		{
+		     if(r == pad+1 && c == pad+1)
+			 {
+			    cout << greeting;
+				c += greeting.size();
+			 } 
+			 else
+			 {
+			   if(r==0 || r == rows-1 || c == 0 || c == cols-1)
+			       cout << "*";
+			   else 
+			       cout << " ";
+			   c++;
+			 }
+		}
+		cout << endl;
+	}
+	cout << endl;
+    return 0;
+}
